2d556138eb27cf1df261a0d4cb1f5b47b8a45a93
Nadeem Khan <nadeemkhanrtm@gmail.com>
2025-08-31 14:56:04 +0530

    refactor: custom delimiter logic and optimize it

--- str-calculator/index.ts
+++ str-calculator/index.ts
@@ -2,29 +2,32 @@ export const stringCalculator = (numbers: string): number => {
   if (numbers === "") {
     return 0;
   }
-  let separatorRg = /,|\n/;
+  let separatorRg = `,|\n`;
   if (numbers.startsWith("//")) {
     const newlineIndex = numbers.indexOf("\n");
     if (newlineIndex !== -1) {
       let delimiterString = numbers.substring(2, newlineIndex);
 
       if (delimiterString.startsWith("[") && delimiterString.endsWith("]")) {
-
-        let delimiter = delimiterString
-          .substring(1, delimiterString.length - 1)
-          .replace(/[.*+?^${}()|[\]\\]/g, "\\$&");
-        separatorRg = new RegExp(`,|\n|${delimiter}`, "g");
-        numbers = numbers.substring(newlineIndex + 1);
-      } else {
-        let delimiter = delimiterString;
-        delimiter = delimiter.replace(/[.*+?^${}()|[\]\\]/g, "\\$&");
-        separatorRg = new RegExp(`,|\n|${delimiter}`, "g");
-        numbers = numbers.substring(newlineIndex + 1);
+        delimiterString = delimiterString.substring(
+          1,
+          delimiterString.length - 1
+        );
       }
+
+      // Escape any special characters in the delimiter string to use in the regex.
+      const escapedDelimiter = delimiterString.replace(
+        /[.*+?^${}()|[\]\\]/g,
+        "\\$&"
+      );
+      // Add the custom delimiter to the regex pattern.
+      separatorRg += `|${escapedDelimiter}`;
+      // Update the string to only contain the numbers.
+      numbers = numbers.substring(newlineIndex + 1);
     }
   }
 
-  const numberSplitByComma = numbers.split(separatorRg);
+  const numberSplitByComma = numbers.split(new RegExp(separatorRg, "g"));
   let sum = 0;
   for (let i = 0; i < numberSplitByComma.length; i++) {
     sum += +numberSplitByComma[i];
