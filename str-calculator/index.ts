38e405efdf93d3b2d19fe2a7f4c4c0af689a38ac
Nadeem Khan <nadeemkhanrtm@gmail.com>
2025-08-31 14:52:41 +0530

    feat: implement custom delimiter with default delimiters

--- str-calculator/index.ts
+++ str-calculator/index.ts
@@ -2,8 +2,29 @@ export const stringCalculator = (numbers: string): number => {
   if (numbers === "") {
     return 0;
   }
-  let sepratorRg = /,|\n/
-  const numberSplitByComma = numbers.split(sepratorRg);
+  let separatorRg = /,|\n/;
+  if (numbers.startsWith("//")) {
+    const newlineIndex = numbers.indexOf("\n");
+    if (newlineIndex !== -1) {
+      let delimiterString = numbers.substring(2, newlineIndex);
+
+      if (delimiterString.startsWith("[") && delimiterString.endsWith("]")) {
+
+        let delimiter = delimiterString
+          .substring(1, delimiterString.length - 1)
+          .replace(/[.*+?^${}()|[\]\\]/g, "\\$&");
+        separatorRg = new RegExp(`,|\n|${delimiter}`, "g");
+        numbers = numbers.substring(newlineIndex + 1);
+      } else {
+        let delimiter = delimiterString;
+        delimiter = delimiter.replace(/[.*+?^${}()|[\]\\]/g, "\\$&");
+        separatorRg = new RegExp(`,|\n|${delimiter}`, "g");
+        numbers = numbers.substring(newlineIndex + 1);
+      }
+    }
+  }
+
+  const numberSplitByComma = numbers.split(separatorRg);
   let sum = 0;
   for (let i = 0; i < numberSplitByComma.length; i++) {
     sum += +numberSplitByComma[i];
